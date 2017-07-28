3590eb99b56e0bbf26855e4389aa3a7619bf74dd
Enrique Galeano <enrique.galeano99@unitec.edu>
2017-07-28 15:45:57 -0600

    EL programa modifica

--- Lab2_11641068/src/lab2_11641068/Lab2_11641068.java
+++ Lab2_11641068/src/lab2_11641068/Lab2_11641068.java
@@ -5,6 +5,9 @@
  */
 package lab2_11641068;
 
+import java.util.ArrayList;
+import javax.swing.JOptionPane;
+
 /**
  *
  * @author enriquejosegaleanotalavera
@@ -15,7 +18,87 @@ public class Lab2_11641068 {
 	 * @param args the command line arguments
 	 */
 	public static void main(String[] args) {
-		// TODO code application logic here
+		ArrayList<usuarios> lista = new ArrayList();
+		String p = " ";
+		usuarios t = new usuarios();
+		while (!p.equals("d")) {
+			p = JOptionPane.showInputDialog("Menu\n "
+					+ "A - Agregar Usuario y edad y Lugar de Nacimiento\n"
+					+ "B - Lista de usuarios\n"
+					+ "C - Modifica usuario \n"
+					+ "D - eliminar usuario\n"
+					+ "d - Salir");
+			if (p.equals("a")) {
+				t = new usuarios();
+				String nombre = " ";
+				int edad = 0;
+				String lnd = " ";
+				String username = " ";
+				String contra = " ";
+				nombre = JOptionPane.showInputDialog("Nombre de usuario");
+				edad = Integer.parseInt(JOptionPane.showInputDialog("Edad"));
+				lnd = JOptionPane.showInputDialog("Lugar De Nacimiento");
+				username = JOptionPane.showInputDialog("Usuario");
+				contra = JOptionPane.showInputDialog("Contraseña");
+				t.setNombre(nombre);
+				t.setEdad(edad);
+				t.setLugarDeNacimiento(lnd);
+				t.setUsername(username);
+				t.setPassword(contra);
+				lista.add(t);
+			}
+			if (p.equals("b")) {
+				String ver = "";
+				for (usuarios ob : lista) {
+					if (ob instanceof usuarios) {
+						ver += " " + lista.indexOf(ob) + "Nombre:  " + ob.getNombre() + "Edad: " + ob.getEdad() + "Lugar de Nacimiento: " + ob.getLugarDeNacimiento() + "\n";
+					}
+				}
+				JOptionPane.showMessageDialog(null, ver);
+			}
+
+			if (p.equals("c")) {
+				int opcion = 0;
+				int pos = 0;
+				int edad;
+				String m = "";
+				String lugar = " ";
+				String usuario = " ";
+				String contra = " ";
+				opcion = Integer.parseInt(JOptionPane.showInputDialog("Que quiere modificar\n"
+						+ "1) Nombre\n"
+						+ "2) Edad\n"
+						+ "3) Lugar de nacimiento\n"
+						+ "4) Username \n"
+						+ "5) Password "));
+				switch (opcion) {
+					case 1:
+						pos = Integer.parseInt(JOptionPane.showInputDialog("Eliga la posicion que quiere modificar"));
+						m = JOptionPane.showInputDialog("Modifique el nombre");
+						lista.get(pos).setNombre(m);
+						break;
+					case 2:
+						pos = Integer.parseInt(JOptionPane.showInputDialog("Eliga la posicion que quiere modificar"));
+						edad = Integer.parseInt(JOptionPane.showInputDialog("Modifique la Edad"));
+						lista.get(pos).setEdad(edad);
+						break;
+					case 3:
+						pos = Integer.parseInt(JOptionPane.showInputDialog("Eliga la posicion que quiere modificar"));
+						lugar = JOptionPane.showInputDialog("Modifique el Lugar De Nacimiento ");
+						lista.get(pos).setLugarDeNacimiento(lugar);
+						break;
+					case 4:
+						pos = Integer.parseInt(JOptionPane.showInputDialog("Eliga la posicion que quiere modificar"));
+						usuario = JOptionPane.showInputDialog("Modifique Username ");
+						lista.get(pos).setUsername(usuario);
+						break;
+					case 5:
+						pos = Integer.parseInt(JOptionPane.showInputDialog("Eliga la posicion que quiere modificar"));
+						contra = JOptionPane.showInputDialog("Modifique Username ");
+						lista.get(pos).setPassword(contra);
+						break;
+				}
+			}
+		}
 	}
-	
 }
--- Lab2_11641068/src/lab2_11641068/usuarios.java
+++ Lab2_11641068/src/lab2_11641068/usuarios.java
@@ -10,25 +10,100 @@ package lab2_11641068;
  * @author enriquejosegaleanotalavera
  */
 public class usuarios {
-	private String nombre;
-	private int edad;
-	private String lugarDeNacimiento;
-	private String username;
-	private String password;
-	private String listaDeAmigos;
-	private String listaDeSolicitudes;
-	private String listaDeMensajesEnviados;
-	private String buzonDeEntrada;
+	public String nombre;
+	public int edad;
+	public String lugarDeNacimiento;
+	public String username;
+	public String password;
+	public String listaDeAmigos;
+	public String listaDeSolicitudes;
+	public String listaDeMensajesEnviados;
+	public String buzonDeEntrada;
 
-	public usuarios(String nombre, int edad, String lugarDeNacimiento, String username, String password, String listaDeAmigos, String listaDeSolicitudes, String listaDeMensajesEnviados, String buzonDeEntrada) {
+	public usuarios() {
+
+	}
+	public usuarios(String Nombre, int edad, String lugarDeNacimiento){
 		this.nombre = nombre;
 		this.edad = edad;
 		this.lugarDeNacimiento = lugarDeNacimiento;
+	}
+
+	
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public int getEdad() {
+		return edad;
+	}
+
+	public void setEdad(int edad) {
+		this.edad = edad;
+	}
+
+	public String getLugarDeNacimiento() {
+		return lugarDeNacimiento;
+	}
+
+	public void setLugarDeNacimiento(String lugarDeNacimiento) {
+		this.lugarDeNacimiento = lugarDeNacimiento;
+	}
+
+	public String getUsername() {
+		return username;
+	}
+
+	public void setUsername(String username) {
 		this.username = username;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
 		this.password = password;
+	}
+
+	public String getListaDeAmigos() {
+		return listaDeAmigos;
+	}
+
+	public void setListaDeAmigos(String listaDeAmigos) {
 		this.listaDeAmigos = listaDeAmigos;
+	}
+
+	public String getListaDeSolicitudes() {
+		return listaDeSolicitudes;
+	}
+
+	public void setListaDeSolicitudes(String listaDeSolicitudes) {
 		this.listaDeSolicitudes = listaDeSolicitudes;
+	}
+
+	public String getListaDeMensajesEnviados() {
+		return listaDeMensajesEnviados;
+	}
+
+	public void setListaDeMensajesEnviados(String listaDeMensajesEnviados) {
 		this.listaDeMensajesEnviados = listaDeMensajesEnviados;
+	}
+
+	public String getBuzonDeEntrada() {
+		return buzonDeEntrada;
+	}
+
+	public void setBuzonDeEntrada(String buzonDeEntrada) {
 		this.buzonDeEntrada = buzonDeEntrada;
 	}
+	@Override
+	public String toString(){
+		return nombre;
+	}
+	
 }
